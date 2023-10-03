c214f8fb75381a067321b912ebb1e2f62338924c
Anis Ismail <anisismail09@gmail.com>
2023-10-03 09:56:36 +0200

    initial upload

---
+++ eval.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+
+from preprocessing import create_fingerprints, decode_ids_np
+from utils import load_rxn_classifier, predict_rxn_type
+from metrics import filter_0, filter_2, filter_4, filter_5, similarity, is_valid_rxn, JSD_with_train
+
+
+"""
+Filters Pipeline
+"""
+pd.options.mode.use_inf_as_na = True
+
+
+def validity_statistics(df):
+    val = df["isValid"].mean()
+    fil0 = df["Filter_0"].mean()
+    fil2 = df["Filter_2"].mean()
+    fil4 = df["Filter_4"].mean()
+    fil5 = df["Filter_5"].mean()
+    return val, fil0, fil2, fil4, fil5
+
+
+def filters_pipeline(df):
+    # run the molecular validation first
+    df["isValid"] = df["decoded_smiles"].apply(is_valid_rxn)
+    print(df["isValid"].sum()/df.shape[0])
+    # run the heuristics filters
+    df["Filter_0"] = df.apply(lambda x: filter_0(
+        x["decoded_smiles"]) if x["isValid"] else False, axis=1)
+    df["Filter_2"] = df.apply(lambda x: filter_2(
+        x["decoded_smiles"]) if x["isValid"] else False, axis=1)
+    df["Filter_4"] = df.apply(lambda x: filter_4(
+        x["decoded_smiles"]) if x["isValid"] else False, axis=1)
+    df["Filter_5"] = df.apply(lambda x: filter_5(
+        x["decoded_smiles"]) if x["isValid"] else False, axis=1)
+    return df
+
+
+def process_results(data, tokenizer, config, use_filters=True):
+    df = pd.DataFrame()
+    # decode ids into smiles
+    df["decoded_smiles"] = np.apply_along_axis(decode_ids_np, 1, data)
+    # run the decoded smiles into the filters pipeline
+    df = filters_pipeline(df)
+    # transform the data into fingerprints
+    df, fingerprints, strings_decoded = create_fingerprints(
+        df, tokenizer, use_filters, config)
+    return df, fingerprints, strings_decoded
+
+
+def generate_metrics_evaluation(generated_file, centroids, centroids_strings, tokenizer, config):
+    with open(generated_file, "r", encoding="utf-8") as f:
+        gen_data = np.loadtxt(f)
+    df, gen_fingerprints, gen_strings = process_results(
+        gen_data, tokenizer, config, )
+    rxn_classifier = load_rxn_classifier(config)
+    rxn_pred = predict_rxn_type(rxn_classifier, gen_fingerprints)
+    avg_similarity, sims = similarity(
+        gen_fingerprints, centroids, metric="jaccard")
+    avg_str_similarity, sims_str = similarity(
+        gen_strings, centroids_strings, metric="cosine")
+    jsd = JSD_with_train(rxn_pred)
+    valid, filter0, filter2, filter4, filter5 = validity_statistics(df)
+
+    return (jsd, avg_similarity, avg_str_similarity, valid,
+            filter0, filter2, filter4, filter5,
+            df, rxn_pred, sims, gen_fingerprints)
+
+
+def generate_metrics_comparison(generated_file, centroids, centroids_strings, tokenizer, config):
+    with open(generated_file, "r", encoding="utf-8") as f:
+        gen_data = np.loadtxt(f)
+
+    def process_and_print_results(use_filters):
+        df, gen_fingerprints, gen_strings = process_results(
+            gen_data, tokenizer, config, use_filters=use_filters)
+        rxn_classifier = load_rxn_classifier(config)
+        rxn_pred = predict_rxn_type(rxn_classifier, gen_fingerprints)
+        avg_similarity, _ = similarity(
+            gen_fingerprints, centroids, metric="jaccard")
+        avg_str_similarity, _ = similarity(
+            gen_strings, centroids_strings, metric="cosine")
+        jsd = JSD_with_train(rxn_pred)
+        val, filter0, filter2, filter4, filter5 = validity_statistics(df)
+
+        filter_status = "Using Filters" if use_filters else "No Filters"
+        print(filter_status, jsd, avg_similarity, avg_str_similarity,
+              val, filter0, filter2, filter4, filter5)
+
+        return df, rxn_pred, gen_fingerprints
+
+    df_old, rxn_pred_old, gen_fingerprints1 = process_and_print_results(
+        use_filters=False)
+    df, rxn_pred, gen_fingerprints2 = process_and_print_results(
+        use_filters=True)
+
+    return df_old, rxn_pred_old, df, rxn_pred, gen_fingerprints1, gen_fingerprints2
---
+++ main.py
@@ -0,0 +1,111 @@
+import os
+import argparse
+import json
+import sys
+import numpy as np
+import torch
+
+from train import LSTMLMTrainer, VAETrainer
+from generate import LSTMLMGenerator, VAEGenerator
+
+parser = argparse.ArgumentParser(description="ChemRxnAIGen")
+
+parser.add_argument("--config", default=None, help='Configuration file path')
+parser.add_argument('--save_dir', type=str,
+                    help="Save directory")
+parser.add_argument("--data_dir", type=str,
+                    help="Path to the data directory containing the dataset")
+parser.add_argument("--train_path", type=str, default="/train/train_targets_ids_200.data",
+                    help="Path to the data directory containing the train dataset")
+parser.add_argument("--val_path", type=str, default="validation/validation_targets_ids_200.data",
+                    help="Path to the data directory containing the val dataset")
+parser.add_argument("--gene_path", type=str, default="/train/v2.4/gene.data",
+                    help="Path to the data directory containing the generated dataset")
+parser.add_argument("--model", type=str, default="LSTM", choices=["LSTM", "VAE"],
+                    help="model to train ")
+parser.add_argument('--n_samples', type=int, default=39579, metavar='N',
+                    help='number of samples generated per time (default: 39579)')
+parser.add_argument("--mode", type=str, default="train", choices=["train", "generate"],
+                    help="mode for model to train or to generate new reaction ")
+parser.add_argument('--vocab_size', type=int, default=56, metavar='N',
+                    help='vocabulary size (default: 56)')
+parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                    help='batch size (default: 64)')
+parser.add_argument('--load_path', type=str, default='',
+                    help='pretrained model path')
+parser.add_argument('--seq_len', type=int, default=200,
+                    help='Generator sequence length (default: 200)')
+parser.add_argument('--no_cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='number of training epochs')
+parser.add_argument('--seed', type=int, default=42,
+                    help='random seed')
+
+
+parser.add_argument('--gen_lr', type=float, default=1e-3, metavar='LR',
+                    help='learning rate of generator optimizer (default: 1e-3)')
+parser.add_argument('--g_embed_dim', type=int, default=64,
+                    help='Generator embedding dimension (default: 64)')
+parser.add_argument('--g_hidden_dim', type=int, default=256,
+                    help='Generator hidden dimension (default: 256)')
+parser.add_argument('--g_num_layers', type=int, default=4,
+                    help='Number of generator layers (default: 4)')
+parser.add_argument('--g_dropout_prob', type=float, default=0.5,
+                    help='Generator dropout probability (default: 0.5)')
+parser.add_argument('--g_weight_decay', type=float, default=0.0005,
+                    help='Generator weight decay (default: 0.0005)')
+
+# VAE
+parser.add_argument('--momentum', type=float, default=0.0, help='sgd momentum')
+parser.add_argument('--warm_up', type=int, default=10,
+                    help='number of annealing epochs')
+parser.add_argument('--kl_start', type=float, default=1.0,
+                    help='starting KL weight')
+parser.add_argument('--aggressive', type=int, default=0,
+                    help='apply aggressive training when nonzero, reduce to vanilla VAE when aggressive is 0')
+parser.add_argument('--enc_nh', type=int, default=1024,
+                    help='encoder hidden size')
+parser.add_argument('--dec_nh', type=int, default=1024,
+                    help='decoder hidden size')
+parser.add_argument('--dec_dropout_in', type=float,
+                    default=0.5, help='decoder input dropout')
+parser.add_argument('--dec_dropout_out', type=float,
+                    default=0.5, help='decoder output dropout')
+parser.add_argument("--clip_grad", type=int, default=5)
+parser.add_argument("--decay_epoch", type=int, default=2)
+parser.add_argument("--lr_decay", type=float, default=0.5)
+parser.add_argument("--max_decay", type=int, default=5)
+
+# Parse the arguments
+config = parser.parse_args()
+if config.config is not None:
+    with open(config.config, 'r', encoding='utf-8') as file:
+        config = json.load(file)
+        config = {param: value for _, params in config.items()
+                  for param, value in params.items()}
+else:
+    print("Arguments are required when configuration file is not provided")
+    parser.print_help()
+    sys.exit(1)
+
+config.cuda = not config.no_cuda and torch.cuda.is_available()
+torch.manual_seed(config.seed)
+np.random.seed(config.seed)
+if config.cuda:
+    torch.cuda.manual_seed(config.seed)
+    torch.backends.cudnn.deterministic = True
+
+if config["mode"] == "train":
+    if config["model"] == "LSTM":
+        trainer = LSTMLMTrainer(config=config)
+    elif config["model"] == "VAE":
+        trainer = VAETrainer(config=config)
+    trainer.train()
+
+elif config["mode"] == "generate":
+    if config["model"] == "LSTM":
+        generator = LSTMLMGenerator(config=config)
+    elif config["model"] == "VAE":
+        generator = VAEGenerator(config=config)
+    generator.generate_samples()
