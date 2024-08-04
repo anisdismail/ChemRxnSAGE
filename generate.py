2a52f207ca0c273c951fcdf829550e19bd1a4c82
ANIS ISMAIL <anisismail09@gmail.com>
2024-08-04 21:37:07 +0200

    added generate script

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from models.LSTM.LSTM_LM import LSTM_LM
 from models.VAE.VAE import VAE
 from models.VAE.LSTM_Decoder import LSTMDecoder
@@ -9,18 +10,24 @@ import sentencepiece as spm
 class LSTMLMGenerator:
     def __init__(self, config):
         self.config = config
-        self.generator = LSTM_LM(config['vocab_size'], config['LSTM_embed_dim'],
-                                 config['LSTM_hidden_dim'], config['LSTM_num_layers'],
-                                 config['cuda'], config['LSTM_dropout_prob'])
-
-        self.generator.load_state_dict(torch.load(config["save_path"]))
+        spm.SentencePieceTrainer.train(
+            "--input=Liu_Kheyer_Retrosynthesis_Data/vocab2.txt --model_prefix=m  --user_defined_symbols=[BOS],[EOS],[PAD],. --vocab_size=56 --bos_id=-1 --eos_id=-1")
+        self.tokenizer = spm.SentencePieceProcessor()
+        self.tokenizer.load('m.model')
+        self.PAD_TOKEN = self.tokenizer.encode_as_ids("[PAD]")[1]
+        self.BOS_TOKEN = self.tokenizer.encode_as_ids("[BOS]")[1]
+        self.EOS_TOKEN = self.tokenizer.encode_as_ids("[EOS]")[1]
+        self.generator = LSTM_LM(vocab_size=config['vocab_size'], embedding_dim=config['LSTM_embed_dim'],
+                                 hidden_dim=config['LSTM_hidden_dim'], num_layers=config['LSTM_num_layers'],
+                                 use_cuda=config['cuda'], dropout_prob=config['LSTM_dropout_prob'],
+                                 BOS_TOKEN=self.BOS_TOKEN, EOS_TOKEN=self.EOS_TOKEN)
 
     def generate_samples(self):
         self.generator.eval()
         samples = []
         for _ in range(int(self.config["n_gen_samples"] / self.config["batch_size"])):
             sample = self.generator.sample(
-                self.config["batch_size"], self.config["seq_len"]).cpu().data.numpy().tolist()
+                self.config["batch_size"], self.config["seq_len"]).cpu().tolist()
             samples.extend(sample)
         with open(self.generated_path, 'w', encoding="utf-8") as fout:
             lines_to_write = [
@@ -38,15 +45,16 @@ class VAEGenerator:
             "--input=Liu_Kheyer_Retrosynthesis_Data/vocab2.txt --model_prefix=m  --user_defined_symbols=[BOS],[EOS],[PAD],. --vocab_size=56 --bos_id=-1 --eos_id=-1")
         self.tokenizer = spm.SentencePieceProcessor()
         self.tokenizer.load('m.model')
-        self.vocab = {self.tokenizer.id_to_piece(
-            i): i for i in range(self.tokenizer.get_piece_size())}
-
+        self.PAD_TOKEN = self.tokenizer.encode_as_ids("[PAD]")[1]
+        self.BOS_TOKEN = self.tokenizer.encode_as_ids("[BOS]")[1]
+        self.EOS_TOKEN = self.tokenizer.encode_as_ids("[EOS]")[1]
         self.encoder = LSTMEncoder(
             self.config, config["vocab_size"], model_init, emb_init)
         self.decoder = LSTMDecoder(
-            self.config, self.vocab, model_init, emb_init)
+            self.config, model_init, emb_init,
+            BOS_token=self.BOS_TOKEN, EOS_token=self.EOS_TOKEN)
         self.vae = VAE(self.encoder, self.decoder, self.config).to(self.device)
-        self.vae.load_state_dict(torch.load(config["save_path"]))
+        self.vae.load_state_dict(self.config["load_path"])
 
     def generate_samples(self):
         self.vae.eval()
